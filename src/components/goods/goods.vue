8b035e6bf104093fcdf4939e73e5e3d6829081ea
violectyc <guestgod@163.com>
2018-02-06 22:05:31 +0800

    header component

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>我是goods</div>
+</template>
+
+<script>
+    export default {
+        name: 'goods'
+    };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>我是ratings</div>
+</template>
+
+<script>
+    export default {
+        name: 'ratings'
+    };
+</script>
+
+<style scoped>
+
+</style>
