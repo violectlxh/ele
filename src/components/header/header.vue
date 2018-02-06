7ac5141b13a30a1d416257b4eba24bb10c88de6d
violectyc <guestgod@163.com>
2018-02-07 00:34:11 +0800

    header component

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -16,9 +16,19 @@
           <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
+        <div v-if="seller.supports" class="support-count">
+          <span class="count">{{seller.supports.length}}个</span>
+          <i class="icon-keyboard_arrow_right"></i>
+        </div>
       </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
   </div>
 </template>
 <script>
@@ -36,9 +46,12 @@ export default {
 <style lang="stylus" scoped>
   @import "../../common/stylus/mixin.styl";
   .header
+    position relative
+    overflow hidden
     color #fff
-    background #000
+    background rgba(7,17,27,.5)
     .content-wrapper
+      position relative
       padding 24px 12px 18px 24px
       font-size 0
       .avatar
@@ -70,25 +83,80 @@ export default {
           line-height 12px
           font-size 12px
         .support
-        .icon
-          display inline-block
+          display block
+          .icon
+            display inline-block
+            vertical-align top
+            width 12px
+            height 12px
+            margin-right 4px
+            background-size 12px 12px
+            background-repeat no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            line-height 12px
+            font-size 10px
+            font-weight 200
+
+      .support-count
+        position absolute
+        right 12px
+        bottom 18px
+        line-height 24px
+        padding 0 8px
+        border-radius 14px
+        background rgba(0,0,0,.2)
+        text-align center
+        .counter
           vertical-align top
-          width 12px
-          height 12px
-          margin-right 4px
-          background-size 12px 12px
-          background-repeat no-repeat
-          &.decrease
-            bg-image('decrease_1')
-          &.discount
-            bg-image('discount_1')
-          &.guarantee
-            bg-image('guarantee_1')
-          &.invoice
-            bg-image('invoice_1')
-          &.special
-            bg-image('special_1')
-        .text
-          line-height 12px
-          font-size 12px
+          font-size 10px
+        .icon-keyboard_arrow_right
+          line-height 24px
+          margin-left 2px
+          font-size 10px
+
+    .bulletin-wrapper
+      position relative
+      height 28px
+      line-height 28px
+      padding 0 22px 0 12px
+      white-space nowrap
+      overflow hidden
+      text-overflow ellipsis
+      background rgba(7,17,27,.2)
+      .bulletin-title
+        display inline-block
+        vertical-align: top
+        margin-top 8px
+        width 22px
+        height 12px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat no-repeat
+      .bulletin-text
+        vertical-align top
+        margin 0 4px
+        font-size 10px
+      .icon-keyboard_arrow_right
+        position absolute
+        font-size 10px
+        right 12px
+        top 8px
+    .background
+      position absolute
+      top 0
+      left 0
+      z-index -1
+      width 100%
+      height 100%
+      filter blur(10px)
 </style>
